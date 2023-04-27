5a35c9914d5d3cce994dccece2553511aee7cb17
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-27 17:32:25 -0300

    feat: buscar produto por id

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { getProdutos, addToCart } from "../controllers/product.controller.js";
+import {
+  getProdutos,
+  addToCart,
+  getProdutoById,
+} from "../controllers/product.controller.js";
 
 const productRoutes = Router();
 productRoutes.get("/produtos", getProdutos);
 productRoutes.post("/pedidos", addToCart);
+productRoutes.get("/produto/:id", getProdutoById);
 
 export default productRoutes;
