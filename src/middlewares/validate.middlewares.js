f67f70349d8ea054503a612918fb328d3aeb2a8c
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-04-28 18:51:57 -0300

    feat: cadastro e login funcionais

---
+++ src/middlewares/validate.middlewares.js
@@ -0,0 +1,11 @@
+export function validate(schema){
+    return (req, res, next) =>{
+        const validacao = schema.validate(req.body, { abortEarly: false })
+    
+        if (validacao.error) {
+            const errors = validacao.error.details.map(detail => detail.message)
+            return res.status(422).send(errors)
+        }
+        next()
+    }
+}
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import authRoutes from './auth.routes.js';
 import productRoutes from './product.routes.js';
+import userRouter from './user.routes.js';
 
 const router = Router();
 router.use(authRoutes);
 router.use(productRoutes);
+router.use(userRouter);
 
 export default router;
