5282c05ddf4c6fe40820b16645a049f08dda7765
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-29 15:40:47 -0300

    feat: rota privada para criação e listagem de pedidos

--- src/routes/pedido.routes.js
+++ src/routes/pedido.routes.js
@@ -5,8 +5,11 @@ import {
 } from "../controllers/pedido.controllers.js";
 import validateSchema from "../middlewares/schema.middleware.js";
 import pedidoSchema from "../schemas/pedido.schema.js";
+import { autentica } from "../middlewares/auth.middlewares.js";
 
 const pedidoRoutes = Router();
+
+pedidoRoutes.use(autentica);
 
 pedidoRoutes.get("/pedidos", listarPedidos);
 pedidoRoutes.post("/pedidos", validateSchema(pedidoSchema), criarPedido);
