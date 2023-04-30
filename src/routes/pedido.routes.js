40048183892b42c0fc8d18f070254780b9024b27
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-30 13:32:00 -0300

    feat: listar pedidos do usuário

--- src/routes/pedido.routes.js
+++ src/routes/pedido.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   criarPedido,
+  getPedido,
   listarPedidos,
 } from "../controllers/pedido.controllers.js";
 import pedidoSchema from "../schemas/pedido.schema.js";
@@ -10,6 +11,7 @@ import { validate } from "../middlewares/validate.middlewares.js";
 const pedidoRoutes = Router();
 
 pedidoRoutes.get("/pedidos", autentica, listarPedidos);
+pedidoRoutes.get("/pedidos/:idPedido", autentica, getPedido);
 pedidoRoutes.post("/pedidos", autentica, validate(pedidoSchema), criarPedido);
 
 export default pedidoRoutes;
