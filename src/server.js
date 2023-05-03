e9a3532ba54e5c8e9f6f0ec9d76138e488c75919
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-05-03 13:24:51 -0300

    ref: dotenv movido para o início

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@ import express from "express";
 import cors from "cors";
 import router from "./routes/index.routes.js";
 import dotenv from "dotenv";
+dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(router);
-dotenv.config();
 
 server.listen(process.env.PORT, () => console.log(`PORT ${process.env.PORT}`));
